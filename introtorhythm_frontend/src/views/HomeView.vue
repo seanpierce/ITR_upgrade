459ca933e46c2c801f86b33dbbb7c47b6190a012
seanpierce <sumler.sean@gmail.com>
2025-09-06 22:46:46 -0700

    Scaffold base components

--- introtorhythm_frontend/src/views/HomeView.vue
+++ introtorhythm_frontend/src/views/HomeView.vue
@@ -1,14 +1,51 @@
 <template>
-  <div>
-    <h1>Shows Today</h1>
-  </div>
+    <div id="live" class="content clearfix">
+
+        <div class="background-container">
+            <img id="bg" :src="bg" alt="">
+        </div>
+
+        <PlayButton />
+
+        <div id="actions">
+            <a href="mailto:hello@introtorhythm.com">hello@introtorhythm.com</a><br>
+            <a href="tel:9718018007">971-801-8007</a><br>
+            <a href="#chat" @click.prevent="goDown()">Chat</a>
+        </div>
+        
+        <Marquee v-if="marqueeText.length && showMarquee" :text="marqueeText" />
+    </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted, onUnmounted } from 'vue'
+import bg from '@/assets/images/i2r-bg-big-tall.png';
+import { nextTick, onMounted, onUnmounted, ref, watch } from 'vue'
 import { useContentStore } from '@/stores/'
+import { storeToRefs } from 'pinia'
+import PlayButton from '@/components/PlayButton.vue';
+import Marquee from '@/components/Marquee.vue';
 
 const contentStore = useContentStore()
+const { marqueeText } = storeToRefs(contentStore)
+const compareMarqueeText = ref<string>('');
+const showMarquee = ref<boolean>(true);
+
+watch(marqueeText, async () => {
+  if (compareMarqueeText.value != marqueeText.value)
+    await forceRemount();
+});
+
+const forceRemount = async () => {
+  showMarquee.value = false;
+  await nextTick(() => {
+    showMarquee.value = true;
+  });
+  compareMarqueeText.value = marqueeText.value;
+}
+
+const goDown = () => {
+  document.getElementById('chat-wrapper')!.scrollIntoView({ block: 'end',  behavior: 'smooth' });
+}
 
 onMounted(() => {
   contentStore.startAutoRefresh()
