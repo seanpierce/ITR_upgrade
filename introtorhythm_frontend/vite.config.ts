d385977501ddc0cf9ddf7a8b4bd6cf4c2698f146
seanpierce <sumler.sean@gmail.com>
2025-09-07 07:43:54 -0700

    Add scss infrastructure

--- introtorhythm_frontend/vite.config.ts
+++ introtorhythm_frontend/vite.config.ts
@@ -10,6 +10,15 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "@/assets/styles/_variables.scss" as *;
+        `
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
