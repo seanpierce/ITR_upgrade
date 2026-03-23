4f1f5ff5804224083f945a18730f239a1afa99e6
seanpierce <sumler.sean@gmail.com>
2026-03-23 14:36:11 -0700

    Update vite config

--- introtorhythm_frontend/vite.config.ts
+++ introtorhythm_frontend/vite.config.ts
@@ -2,18 +2,22 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-import path from 'node:path'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(async ({ mode }) => {
+  const devToolsPlugin =
+    mode === 'development'
+      ? (await import('vite-plugin-vue-devtools')).default()
+      : null
+
+  return {
+    plugins: [
+      vue(),
+      ...(devToolsPlugin ? [devToolsPlugin] : []),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
+  }
 })
